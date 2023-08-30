ae434189d4364a7ac33445d2eec0a09678611a14
Chris Sargent <chris.sargent-CTR@ecstech.com>
2023-08-30 14:27:30 -0400

    Updated run_script.sh with absolute paths CAS 08302023

--- ansible/rhel8_ami_ansible_ps/roles/rhel8_ami_ansible_ps/files/scripts/run_scripts.sh
+++ ansible/rhel8_ami_ansible_ps/roles/rhel8_ami_ansible_ps/files/scripts/run_scripts.sh
@@ -5,25 +5,25 @@ set -e
 # Fix AIDE
 cd /var/lib/aide/ && rm -rf * && \
 cp /etc/aide.conf /etc/aide.conf.ORIG && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_aide_build_database.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_aide_check_audit_tools.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_aide_verify_acls.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_aide_verify_ext_attributes.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_aide_build_database.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_aide_check_audit_tools.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_aide_verify_acls.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_aide_verify_ext_attributes.sh && \
 
 # Fix crypto
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_harden_sshd_ciphers_openssh_conf_crypto_policy.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_harden_sshd_macs_openssh_conf_crypto_policy.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_harden_sshd_ciphers_openssh_conf_crypto_policy.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_harden_sshd_macs_openssh_conf_crypto_policy.sh && \
 
 # Fix Pam and password related hits
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_accounts_password_pam_pwhistory_remember_password_auth.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_accounts_password_set_min_life_existing.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_accounts_passwords_pam_faillock_deny.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_display_login_attempts.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_no_empty_passwords.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_accounts_password_pam_pwhistory_remember_password_auth.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_accounts_password_set_min_life_existing.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_accounts_passwords_pam_faillock_deny.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_display_login_attempts.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_no_empty_passwords.sh && \
 
 # Fix permission related hits 
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_accounts_users_home_files_permissions.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_kernel_module_bluetooth_disabled.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_sssd_enable_smartcards.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_sssd_offline_cred_expiration.sh && \
-bash_remediate_scripts/xccdf_org.ssgproject.content_rule_usbguard_generate_policy.sh
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_accounts_users_home_files_permissions.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_kernel_module_bluetooth_disabled.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_sssd_enable_smartcards.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_sssd_offline_cred_expiration.sh && \
+/root/scripts/bash_remediate_scripts/xccdf_org.ssgproject.content_rule_usbguard_generate_policy.sh
